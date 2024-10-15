9c528f1e0c5a80a2a3370a4f820a87ab56886f45
Sindu02 <113755359+Sindu02@users.noreply.github.com>
2024-10-15 07:43:31 +0530

    Create Con.java

---
+++ Con.java
@@ -0,0 +1,20 @@
+package ASimulatorSystem;
+
+import java.sql.*;  
+
+public class Con{
+    Connection c;
+    Statement s;
+    public Con(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","root");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
